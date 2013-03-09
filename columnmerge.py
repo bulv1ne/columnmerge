b68cd5f712d1d05c5bb26d7197c64fc1b6ad5029
Niels Lemmens <niels.lemmens@dinwebbstrateg.se>
2013-03-09 11:44:01 +0100

    Update columnmerge.py
    Change def so it converts the data to the correct Column class

--- columnmerge.py
+++ columnmerge.py
@@ -11,16 +11,19 @@ class Column:
     def __exit__(self, type, value, traceback):
         self.locked = False
 
-def iter_column(columns):
-    for column in columns:
-        if column.locked:
-            continue
-        # Automatic lock and unlock column
-        with column:
-            for row in column.rows:
-                yield (row,)
-                for i in iter_column(columns):
-                    yield (row,) + i
+def iter_column(data):
+    columns = map(Column, data)
+    def _iter(columns):
+        for column in columns:
+            if column.locked:
+                continue
+            # Automatic lock and unlock column
+            with column:
+                for row in column.rows:
+                    yield (row,)
+                    for i in _iter(columns):
+                        yield (row,) + i
+    return _iter(columns)
 
 
 if __name__ == "__main__":
@@ -31,9 +34,7 @@ if __name__ == "__main__":
         ['C1','C2']
     ]
     
-    # Convert data to columns
-    columns = map(Column, data)
-    
     # Go over the iteration and print the result
-    for x in iter_column(columns):
-        print x
+    for x in iter_column(data):
+            print ' '.join(x)
+
